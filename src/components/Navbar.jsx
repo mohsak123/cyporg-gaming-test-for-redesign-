a04b996021df29e1d6e2dafde77104b3844a40a7
mohsak13 <mohammedshakokah@gmail.com>
2024-12-03 13:42:28 +0300

    testing relative path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
         className="flex transition-all duration-150 relative h-[80px] lg:h-[70px] mx-auto items-center justify-between px-[30px] lg:px-[12px] w-full lg:w-[960px] xl:w-[1140px] 2xl:w-[1320px] max-w-full pt-[0] lg:pt-[40px]"
       >
         <div className="xl:border-r xl:border-bgSecondary xl:w-[218px] 2xl:w-[262px] ">
-          <img src="/src/assets/images/logo.png" className=" max-w-full" />
+          <img src="./src/assets/images/logo.png" className=" max-w-full" />
         </div>
         <div className="hidden lg:pl-8 xl:pl-14 2xl:pl-16 lg:flex justify-between items-center flex-1">
           <form className="relative">
